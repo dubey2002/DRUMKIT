3316d0caff3352ea27fda8779e98d09504ffb24b
Anurag Dubey <98604732+dubey2002@users.noreply.github.com>
2022-05-29 19:55:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 document.querySelector(".A").addEventListener("click",function(){
-  var audio=new Audio("sounds/crash.mp3");
+  var audio=new Audio("crash.mp3");
   audio.play();
 
 });
 
 document.querySelector(".N").addEventListener("click",function(){
-  var audio=new Audio("sounds/1.wav");
+  var audio=new Audio("1.wav");
   audio.play();
 });
 document.querySelector(".U").addEventListener("click",function(){
-  var audio=new Audio("sounds/2.mp3");
+  var audio=new Audio("2.mp3");
   audio.play();
 });
 
 document.querySelector(".R").addEventListener("click",function(){
-  var audio=new Audio("sounds/tom-1.mp3");
+  var audio=new Audio("tom-1.mp3");
   audio.play();
 });
-document.querySelector("..A").addEventListener("click",function(){
-  var audio=new Audio("sounds/3.mp3");
+document.querySelector(".a").addEventListener("click",function(){
+  var audio=new Audio("3.mp3");
   audio.play();
 });
 document.querySelector(".G").addEventListener("click",function(){
-  var audio=new Audio("sounds/tom-3.mp3");
+  var audio=new Audio("tom-3.mp3");
   audio.play();
 });
 document.querySelector(".D").addEventListener("click",function(){
-  var audio=new Audio("sounds/4.mp3");
+  var audio=new Audio("4.mp3");
   audio.play();
 });
