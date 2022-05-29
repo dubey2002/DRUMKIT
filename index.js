958e059f29d763b76daf3a84b635a32537930fb2
Anurag Dubey <98604732+dubey2002@users.noreply.github.com>
2022-05-29 19:27:27 +0530

    Add initial drum_kit files

---
+++ index.js
@@ -0,0 +1,31 @@
+document.querySelector(".A").addEventListener("click",function(){
+  var audio=new Audio("sounds/crash.mp3");
+  audio.play();
+
+});
+
+document.querySelector(".N").addEventListener("click",function(){
+  var audio=new Audio("sounds/1.wav");
+  audio.play();
+});
+document.querySelector(".U").addEventListener("click",function(){
+  var audio=new Audio("sounds/2.mp3");
+  audio.play();
+});
+
+document.querySelector(".R").addEventListener("click",function(){
+  var audio=new Audio("sounds/tom-1.mp3");
+  audio.play();
+});
+document.querySelector("..A").addEventListener("click",function(){
+  var audio=new Audio("sounds/3.mp3");
+  audio.play();
+});
+document.querySelector(".G").addEventListener("click",function(){
+  var audio=new Audio("sounds/tom-3.mp3");
+  audio.play();
+});
+document.querySelector(".D").addEventListener("click",function(){
+  var audio=new Audio("sounds/4.mp3");
+  audio.play();
+});
